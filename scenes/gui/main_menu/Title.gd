e70779710d096eb7d3594c35f2772e5cd6ffd895
dionisis2014 <toulatosdionisis@gmail.com>
2022-04-26 20:53:12 +0300

    Added basic main menu.

---
+++ scenes/gui/main_menu/Title.gd
@@ -0,0 +1,7 @@
+extends Node
+
+export var time_fade = 2.0
+
+func _ready():
+	$Tween.interpolate_property($Title, "percent_visible", 0.0, 1.0, time_fade, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
